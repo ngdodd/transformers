35da8551019dcffee0e6366078c25584882552e3
ngdodd <ndodd@asu.edu>
2020-11-09 22:45:20 -0800

    Disable logging for truncated tokens in inputs.

--- examples/multiple-choice/utils_multiple_choice.py
+++ examples/multiple-choice/utils_multiple_choice.py
@@ -582,13 +582,14 @@ def convert_examples_to_features(
                 truncation=True,
                 return_overflowing_tokens=True,
             )
+            """
             if "num_truncated_tokens" in inputs and inputs["num_truncated_tokens"] > 0:
                 logger.info(
                     "Attention! you are cropping tokens (swag task is ok). "
                     "If you are training ARC and RACE and you are poping question + options,"
                     "you need to try to use a bigger max seq length!"
                 )
-
+            """
             choices_inputs.append(inputs)
 
         label = label_map[example.label]
